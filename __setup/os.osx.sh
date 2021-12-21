d87cab9f4468dee30db3a524c9ac3ab13ed6c212
Eric Kincl <erick@standardeventrentals.com>
2021-12-21 14:29:37 -0800

    Updated for OSX 12
    Updated so it should work with a clean install of OSX 12

--- __setup/os.osx.sh
+++ __setup/os.osx.sh
@@ -11,9 +11,12 @@ if [[ $RTN != 0 ]]; then
     exit $RTN
 fi
 
+brewpath=/opt/homebrew/bin/brew
+
 # Tap Homebrew repos
-cat $DIRECTORY/brew.taps | xargs -L 1 brew tap                  # Pull list with `brew tap`
+cat $DIRECTORY/brew.taps | xargs -L 1 $brewpath tap                  # Pull list with `brew tap`
 
 # Install homebrew apps
-cat $DIRECTORY/brew.packages | xargs brew install               # Pull list with `brew leaves`
-cat $DIRECTORY/brew.cask.packages | xargs brew cask install     # Pull list with `brew cask list`
+cat $DIRECTORY/brew.packages | xargs $brewpath install               # Pull list with `brew leaves`
+cat $DIRECTORY/brew.cask.packages | xargs $brewpath install     # Pull list with `brew cask list`
+
